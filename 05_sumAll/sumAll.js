dc6d89a6d40b12c4562678c1f3ffa8e135f766fd
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-07-18 19:19:16 +0100

    Add solution to 05 exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+    if ((start >= 0 && end >= 0) &&
+    (typeof start === "number" && typeof end === "number")
+    ) {
+        let difference = end - start;
+        let result = 0;
+        if (difference > 0) {
+            for (let a = 0; a <= difference; a++) {
+                result += start + a;
+            }
+        } else if (difference === 0) {
+            result = start;
+        } else {
+            for (let a = 0; a >= difference; a--) {
+                result += start + a;
+            }
+        }
+        return result;
+    }
+    return "ERROR";
 };
 
 // Do not edit below this line
