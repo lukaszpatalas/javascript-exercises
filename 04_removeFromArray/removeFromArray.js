d04b709f18ce7ed30402fa77a2c7e62e1ab93d73
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-07-18 16:02:14 +0100

    Add comments helping to resolve the problem

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,3 +1,8 @@
+// Function is taking variables store and forDeletion
+// then use for loop to search forDeletion in store
+// try remove forDeletion from store using splice()
+// return store
+
 const removeFromArray = function() {
 
 };
