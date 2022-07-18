ea121dfbcaf4e55559483e610b8b4999b98602d3
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-07-18 19:29:00 +0100

    Update solution to exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,23 +1,17 @@
 const sumAll = function (start, end) {
-    if ((start >= 0 && end >= 0) &&
-    (typeof start === "number" && typeof end === "number")
-    ) {
-        let difference = end - start;
-        let result = 0;
-        if (difference > 0) {
-            for (let a = 0; a <= difference; a++) {
-                result += start + a;
-            }
-        } else if (difference === 0) {
-            result = start;
-        } else {
-            for (let a = 0; a >= difference; a--) {
-                result += start + a;
-            }
-        }
-        return result;
+    if (start < 0 || end < 0) return "ERROR";
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    if (start > end) {
+        const temp = start;
+        start = end;
+        end = temp;
     }
-    return "ERROR";
+    const difference = end - start;
+    let result = 0;
+    for (let a = 0; a <= difference; a++) {
+        result += start + a;
+    }
+    return result;
 };
 
 // Do not edit below this line
