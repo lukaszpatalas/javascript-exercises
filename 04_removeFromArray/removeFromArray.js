e34fba4d91e28d2cb915fb284e8fc44fe17ba52f
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-07-18 16:49:04 +0100

    Add code to pass first test in 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,8 +3,14 @@
 // try remove forDeletion from store using splice()
 // return store
 
-const removeFromArray = function() {
-
+const removeFromArray = function (store, forDeletion) {
+    for (let a = 0; a < store.length; a++) {
+        if (store[a] === forDeletion) {
+            console.log(store[a]);
+            store.splice(a, 1);
+        }
+    }
+    return store;
 };
 
 // Do not edit below this line
