7909248860688e5541bda476e52e20f0c33e6298
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-07-19 20:13:13 +0100

    Add solution to exercise leapYear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
+// leapYears takes parameter leapYear
+// condition to check - leapYear divisible by 4 and both 100 and 400
+// if true - returns true, else false
 
+const leapYears = function (leapYear) {
+
+    if (leapYear % 4 === 0) {
+        if (leapYear % 100 === 0) {
+            if (leapYear % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
