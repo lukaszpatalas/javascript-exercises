31f0b2689a4d52e77790ff61e591047632e883dd
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-07-20 21:09:37 +0100

    Add functions to calculate temperatures

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
 const ftoc = function (fahTemp) {
   let celsiusTemp = ((fahTemp - 32) * (5 / 9));
-  celsiusTemp % 1 === 0 ? celsiusTemp = celsiusTemp : celsiusTemp.toFixed(1);
+  celsiusTemp % 1 != 0 ? celsiusTemp = parseFloat(celsiusTemp.toFixed(1)) : celsiusTemp;
   return celsiusTemp;
 };
 
 const ctof = function (celTemp) {
-
+  let fahrenheitTemp = (celTemp * (9 / 5)) + 32;
+  fahrenheitTemp % 1 != 0 ? fahrenheitTemp = parseFloat(fahrenheitTemp.toFixed(1)) : fahrenheitTemp;
+  return fahrenheitTemp;
 };
 
 // Do not edit below this line
