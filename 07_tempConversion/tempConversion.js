ecc5b0a839be16cacaccfb56dd99c47e6e713117
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-07-20 21:36:47 +0100

    Make functions more readible

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,16 @@
 const ftoc = function (fahTemp) {
   let celsiusTemp = ((fahTemp - 32) * (5 / 9));
-  celsiusTemp % 1 != 0 ? celsiusTemp = parseFloat(celsiusTemp.toFixed(1)) : celsiusTemp;
+  if (celsiusTemp % 1 != 0) {
+    celsiusTemp = parseFloat(celsiusTemp.toFixed(1));
+  }
   return celsiusTemp;
 };
 
 const ctof = function (celTemp) {
   let fahrenheitTemp = (celTemp * (9 / 5)) + 32;
-  fahrenheitTemp % 1 != 0 ? fahrenheitTemp = parseFloat(fahrenheitTemp.toFixed(1)) : fahrenheitTemp;
+  if (fahrenheitTemp % 1 != 0) {
+    fahrenheitTemp = parseFloat(fahrenheitTemp.toFixed(1));
+  }
   return fahrenheitTemp;
 };
 
