4a0d80838dc1e751a1f59bea8052c14bafbd8211
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-07-21 12:54:41 +0100

    Modify solution to temperature conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,17 +1,9 @@
 const ftoc = function (fahTemp) {
-  let celsiusTemp = ((fahTemp - 32) * (5 / 9));
-  if (celsiusTemp % 1 != 0) {
-    celsiusTemp = parseFloat(celsiusTemp.toFixed(1));
-  }
-  return celsiusTemp;
+  return Math.round((fahTemp - 32) * (5/9) * 10) / 10;
 };
 
 const ctof = function (celTemp) {
-  let fahrenheitTemp = (celTemp * (9 / 5)) + 32;
-  if (fahrenheitTemp % 1 != 0) {
-    fahrenheitTemp = parseFloat(fahrenheitTemp.toFixed(1));
-  }
-  return fahrenheitTemp;
+  return Math.round(((celTemp * 9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
